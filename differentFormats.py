3563b45793cda4850bafdc2b62e6ab404e19b5e0
LeahBasson <leahbasson54@gmail.com>
2025-04-22 17:25:03 +0200

    :tada:

---
+++ differentFormats.py
@@ -0,0 +1,12 @@
+#possible format options
+
+price = 9000
+num = 0.09
+
+print("2 decimal places: " f"{price:.2f}")
+
+print("Separate the zeros with a comma: " f"{price:,}")
+
+print("Converts to corresponding unicode: " f"{price:c}")
+
+print("Percentage format: " f"{num:.0%}")
